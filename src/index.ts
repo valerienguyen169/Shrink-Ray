652a573f56a0a90d8f79010e3f17f688e050adcb
vynguyn <vy21014nguyn@gmail.com>
2023-03-31 22:44:47 -0500

    Update file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
 import './config'; // Load environment variables
 import 'express-async-errors'; // Enable default error handling for async errors
+import express, { Express } from 'express';
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
+import { registerUser, logIn } from './controllers/UserController';
+import { shortenUrl, deleteLink, getOriginalUrl, getLinkData } from './controllers/LinkController';
+
+const app: Express = express();
+const { PORT, COOKIE_SECRET } = process.env;
+
+const SQLiteStore = connectSqlite3(session);
+
+app.use(
+  session({
+    store: new SQLiteStore({ db: 'sessions.sqlite' }),
+    secret: COOKIE_SECRET,
+    cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours
+    name: 'session',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.json());
+
+app.post('/api/users', registerUser);
+app.post('/api/login', logIn);
+app.get('/api/users/:targetUserId/links', getLinkData);
+app.post('/api/links', shortenUrl);
+app.delete('/api/users/:targetUserId/links/:targetLinkId', deleteLink);
+app.get('/:targetLinkId', getOriginalUrl);
+
+app.listen(PORT, () => console.log(`Listening on port http://127.0.0.1:${PORT}`));
